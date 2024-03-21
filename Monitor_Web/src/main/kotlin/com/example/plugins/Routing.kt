4e12538db7b882d06d874ef8225c6e9152374e0e
Aleorfu <aleorfu@proton.me>
2024-03-21 18:12:01 +0100

    Implemento la vista de bernardo del login

--- Monitor_Web/src/main/kotlin/com/example/plugins/Routing.kt
+++ Monitor_Web/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,12 +1,14 @@
 package com.example.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/static", "static")
         get("/") {
             call.respondRedirect("/login")
         }
